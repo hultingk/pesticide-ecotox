b3e61e29718bbe0fc232285961886fb35d26314a
darstash <darst017@umn.edu>
2025-05-08 11:01:17 -0400

    Finished filtering leps. Filtered pesticides.

--- 01_ecotox-wrangling.R
+++ 01_ecotox-wrangling.R
@@ -41,28 +41,63 @@ species <- ecotox %>%
 # 5. Note gaps for Leps, note variation in addition to mean values
 #########
 
+
 # Filter for lepidopterans
 ## I want this done first for personal reasons... my meta analysis maybe!
-## Split into genus column -- assuming NAs generated are due to family level data, but ned to check
-# 119 at Insecta level...
+## Split into genus column -- NAs generated due to family level data
+## Subfamilies are all ants
 ecotox <- ecotox %>%
   separate(species_scientific_name, into = c("genus", "species"), sep = "^\\S*\\K\\s+")
 
 genus <- ecotox %>%
   count(genus)
 
-# A-L done
-# Not including Insecta
-ecotox %>%
-  filter(Endpoint %in% c("Acrolepia", "Adoxophyes", "Agrotis", "Alabama", "Amyelois", "Anagasta", "Anarsia", "Antheraea", "Anticarsia", "Aroga", "Athetis", "Autographa", "Bombyx", "Cadra", "Catopsilia", "Cerconota", "Chilo", "Choristoneura", "Chrysodeixis", "Cnaphalocrocis", "Cochylis", "Coleotechnites", "Conopomorpha", "Corcyra", "Crocidolomia", "Cydia", "Danaus", "Diatraea", "Dioryctria", "Earias", "Egira", "Elasmopalpus", "Emmalocera", "Ephestia", "Epiphyas", "Epirrita", "Estigmene", "Etiella", "Eublemma", "Galleria", "Glyphodes", "Grapholita", "Helicoverpa", "Heliothis", "Hellula", "Homoeosoma", "Homona", "Hydria", "Hypena", "Issoria", "Itame", "Keiferia", "Lacanobia", "Lampides", "Leguminivora", "Lepidoptera", "Leucinodes", "Leucoptera", "Lobesia", "Lymantria", ""))
+## Not including Insecta -- mostly population level effect + some development and some avoidance effects
+ecotox_lep <- ecotox %>%
+  filter(genus %in% c("Acrolepia", "Adoxophyes", "Agrotis", "Alabama", "Amyelois", "Anagasta", "Anarsia", "Antheraea", "Anticarsia", "Aroga", "Athetis", "Autographa", "Bombyx", "Cadra", "Catopsilia", "Cerconota", "Chilo", "Choristoneura", "Chrysodeixis", "Cnaphalocrocis", "Cochylis", "Coleotechnites", "Conopomorpha", "Corcyra", "Crocidolomia", "Cydia", "Danaus", "Diatraea", "Dioryctria", "Earias", "Egira", "Elasmopalpus", "Emmalocera", "Ephestia", "Epiphyas", "Epirrita", "Estigmene", "Etiella", "Eublemma", "Galleria", "Glyphodes", "Grapholita", "Helicoverpa", "Heliothis", "Hellula", "Homoeosoma", "Homona", "Hydria", "Hypena", "Issoria", "Itame", "Keiferia", "Lacanobia", "Lampides", "Leguminivora", "Lepidoptera", "Leucinodes", "Leucoptera", "Lobesia", "Lymantria", "Mamestra", "Manduca", "Maruca", "Mythimna", "Neoleucinodes", "Noctuidae", "Ostrinia", "Pandemis", "Pectinophora", "Peridroma", "Phauda", "Phthorimaea", "Phyllocnistis", "Phyllonorycter", "Pieris", "Planotortrix", "Platynota", "Plutella", "Pseudohypatopa", "Pyralidae", "Rhopobota", "Samia", "Sceliodes", "Scirpophaga", "Sesamia", "Sparganothis", "Spilarctia", "Spodoptera", "Striacosta", "Tineola", "Tortricidae", "Trichoplusia", "Tuta", "Vanessa", "Zeuzera"))
 
-# Check types of endpoints -- NA endpoint is concerning, LOEL/NOEL may be useful...
-ecotox %>%
+
+# Filter for response
+## Check types of endpoints -- NA endpoint is concerning, LOEL/NOEL may be useful...
+ecotox_lep %>%
   count(endpoint) %>%
   print(n = nrow(.))
 
+## Check types of effects for LD/LC50 -- all mortality, this is good
+ecotox_lep %>%
+  filter(endpoint %in% c("LD50", "LC50")) %>%
+  count(effect) %>%
+  print(n = nrow(.))
+
 ## filtering for chosen response measured -- narrows it down a lot
-ecotox <- ecotox %>%
+ecotox_lep50 <- ecotox_lep %>%
   filter(endpoint %in% c("LD50", "LC50")) %>%
   filter(effect %in% c("Mortality"))
 
+## Filter for only active ingredient -- we don't know we is lethal in formulations, CHECK what total is
+ecotox_lep50 <- ecotox_lep50 %>%
+  filter(conc_1_type_author == "Active ingredient")
+
+
+# Filter for pesticides
+chemical <- ecotox_lep50 %>%
+  count(chemical_name)
+
+## Rename chemicals -- two different DDTs, called both DDT
+ecotox_lep50 <- ecotox_lep50 %>%
+  mutate(chemical_name_common = recode(chemical_name, "(1E)-N-[(6-Chloro-3-pyridinyl)methyl]-N'-cyano-N-methylethanimidamide" = "acetamiprid", "(1E)-N-[(6-Chloro-3-pyridinyl)methyl]-N-ethyl-N'-methyl-2-nitro-1,1-ethenediamine
+" = "nitenpyram", "(2E)-1-[(6-Chloro-3-pyridinyl)methyl]-N-nitro-2-imidazolidinimine" = "imidacloprid", "(4R,4aR,5R,7S,9S,10S,10aR,11S,12S)-2-Amino-1,4,4a,5,9,10-hexahydro-12-(hydroxymethyl)-5,9:7,10a-dimethano-10aH-[1,3]dioxocino[6,5-d]pyrimidine-4,7,10,11,12-pentol" = "tetrodotoxin", "(5alpha,6alpha)-7,8-Didehydro-4,5-epoxy-17-methylmorphinan-3,6-diol, Sulfate (2:1)" = "morphine sulfate", "1,1'-(2,2,2-Trichloroethylidene)bis[4-chlorobenzene]" = "DDT", "1-Chloro-2-(2,2,2-trichloro-1-(4-chlorophenyl)ethyl)benzene" = "DDT", "1-[[2-[2-Chloro-4-(4-chlorophenoxyl)phenyl]-4-methyl-1,3-dioxolan-2-yl]methyl]-1H-1,2,4-triazole" = "difenoconazole", "3-[(2-Chloro-5-thiazolyl)methyl]tetrahydro-5-methyl-N-nitro-4H-1,3,5-oxadiazin-4-imine" = "thiamethoxam", "3-[Benzoyl(methyl)amino]-N-[2-bromo-4-(1,1,1,2,3,3,3-heptafluoropropan-2-yl)-6-(trifluoromethyl)phenyl]-2-fluorobenzamide" = "broflanilide", "N''-Methyl-N-nitro-N'-[(tetrahydro-3-furanyl)methyl]guanidine" = "dinotefuran", "N-[[[2,5-Dichloro-4-(1,1,2,3,3,3-hexafluoropropoxy)phenyl]amino]carbonyl]-2,6-difluorobenzamide" = "lufenuron", "N-[[[3,5-Dichloro-4-(1,1,2,2-tetrafluoroethoxy)phenyl]amino]carbonyl]-2,6-difluorobenzamide" = "hexaflumuron", "N2-[1,1-Dimethyl-2-(methylsulfonyl)ethyl]-3-iodo-N1-[2-methyl-4-[1,2,2,2-tetrafluoro-1-(trifluoromethyl)ethyl]phenyl]-1,2-benzenedicarboxamide" = "flubendiamide", "Potassium cyanide (K(CN))" = "potassium cyanide", "[C(E)]-N-[(2-Chloro-5-thiazolyl)methyl]-N'-methyl-N''-nitroguanidine" = "clothianidin", "[N(Z)]-N-[3-[(6-Chloro-3-pyridinyl)methyl]-2-thiazolidinylidene]cyanamide" = "thiacloprid", "[N-[2-[(Dithiocarboxy)amino]ethyl]carbamodithioato(2-)-kappaS,kappaS']manganese mixt. with [N-[2-[(dithiocarboxy)amino]ethyl]carbamodithioato(2-)-kappaS,kappaS']zinc" = "mixture"))
+
+## Filtering for pesticides and no mixtures
+ecotox_lep50 <- ecotox_lep50 %>%
+  filter(chemical_name_common != "mixture" & chemical_name_common != "morphine sulfate")
+
+## MISSING LD50 values from these papers... for example, Zheng et al.
+## LESS values than in the Hall synthesis from 2021
+## Hall synthesis has stricter standards than us (potentially)
+  # a. Larval dose (topical studies) or diet concentration (dietary studies) units were not provided or could not be deciphered.
+  # b. No ‘susceptible’ (i.e., non-resistant) lab populations were tested.
+  # c. Single active ingredient compounds were not tested.
+  # d. An appropriate solvent control was not utilized.
+  # e. Insecticide solvent carrier for toxicity bioassays not provided.
+  # f. Published in a language other than English.
